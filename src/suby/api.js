117d968af05bac45069f7aaf93bce4ef0870650c
sujayyy <150987692+sujaytumu@users.noreply.github.com>
2025-08-12 17:51:43 +0530

    Update api.js

--- src/suby/api.js
+++ src/suby/api.js
@@ -1,3 +1,2 @@
-// export const API_URL = "https://backend-nodejs-suby.onrender.com"
-
-export const API_URL = "http://localhost:4000"
+export const API_URL = "https://yummy-food-app-backend.onrender.com"
+// export const API_URL = "http://localhost:4000"
